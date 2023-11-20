29b67a729373b6637262c1b5b10a9d73be24e61d
Phan Định <vonhanh271@gmail.com>
2023-11-20 22:45:51 +0700

    Create sourccode

---
+++ DoAn/QuanLyNhaHang/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QuanLyNhaHang
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Chạy trang nhân viên Admin đầu tiên
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { Controller = "DangNhap", action = "DangNhap", id = UrlParameter.Optional },
+                namespaces: new string[] { "QuanLyNhaHang.Areas.NhanVien.Controllers" }
+            ).DataTokens.Add("area", "NhanVien");
+
+        }
+    }
+}
+
